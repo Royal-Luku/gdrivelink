2bcd0707b88e6fa323f42f0d6d9d861f977e5b3e
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-02-03 16:32:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://gdrivelink.vercel.app/"
+URL = "https://gdrivelink.vercel.app"
 bot = Bot(TOKEN)
 
 
